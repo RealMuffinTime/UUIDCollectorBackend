ccdbb75f79400aef93eea830f8db9a56e53eeb37
Northernside <github@northernsi.de>
2023-03-03 23:12:25 +0100

    📰 Better info endpoints

--- index.ts
+++ index.ts
@@ -16,6 +16,10 @@ app.get("/api/user/index/:uuid/:username", ({params}) => {
     db.addUser(user);
 });
 
-app.get("/api/user/total", () => {
-    return db.getUsers();
+app.get("/api/user/total/length", () => {
+    return {status: 200, length: db.getUsers().length};
+});
+
+app.get("/api/user/total/json", () => {
+    return {status: 200, users: db.getUsers()};
 });
