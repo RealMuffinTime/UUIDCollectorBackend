f3c3457a1cc8558e0134b4bdc7952a1f8b9e99ca
RealMuffinTime <realmuffintime@gmail.com>
2023-11-23 20:45:18 +0100

    return valid uploaded and total uuids on donate route

--- index.ts
+++ index.ts
@@ -13,6 +13,8 @@ app.post("/api/donate/:key", ({params, body, set}) => {
     const key = db.getKey(params.key);
     if (!key) {set.status = 401; return {status: 401, message: "Invalid authentication key."};}
 
+    const before_length = db.getUsers().length;
+
     const users = JSON.parse(body as any).users;
     for (let user in users) {
         const userObj: User = {
@@ -23,16 +25,18 @@ app.post("/api/donate/:key", ({params, body, set}) => {
 
         db.addUser(userObj, key.owner);
     }
+
+    const after_length = db.getUsers().length;
     
     console.log("Retrieved new UUIDs by " + key.owner + ".")
 
-    return {status: 200, message: "Thank you!", length: users.length};
+    return {status: 200, message: "Thank you!", valid: after_length - before_length, length: after_length};
 });
 
 app.get("/api/key/:key", ({params, set}) => {
     const key = db.getKey(params.key);
     if (!key) {set.status = 401; return {status: 401, message: "Invalid authentication key."};}
-
+    
     return {status: 200, length: db.getUsers().length};
 });
 
